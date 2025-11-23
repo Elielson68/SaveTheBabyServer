7e3a5f40a7e4a799f1cb71cb693a4d0ab12899cf
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-11-23 17:23:30 -0300

    fix: corrige vídeos do streamingassets

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
+
+
+# Rota específica para StreamingAssets
+@app.route('/StreamingAssets/<path:filename>')
+def serve_streaming_assets(filename):
+    streaming_assets_path = os.path.join('static', 'StreamingAssets', filename)
+
+    # Verifica se o arquivo existe
+    if not os.path.isfile(streaming_assets_path):
+        return "File not found", 404
+
+    return send_file(streaming_assets_path)
 
 @app.route('/')
 def home():
